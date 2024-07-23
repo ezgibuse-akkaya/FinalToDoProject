cdbd0db908e0406f3e27b5dabb2c6281830615bc
Ezgi Buse Işık <ezgibuse159@gmail.com>
2024-07-23 19:52:19 +0300

    added backend
    added backend javaspringboot entegration

---
+++ src/main/java/com/ezgibuseisik/todolistproject/model/ToDo.java
@@ -0,0 +1,40 @@
+package com.ezgibuseisik.todolistproject.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String task;
+    private boolean completed;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
